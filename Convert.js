c47f5a54fe10554e8fce32dd7061bf6cec8c1dca
Ongvn <duykl159vn@gmail.com>
2018-01-04 12:01:07 +0700

    Create Convert.js

---
+++ Convert.js
@@ -0,0 +1,15 @@
+var tab_text;
+var data_type = 'data:application/vnd.ms-excel';
+
+function CreateTable(Obj) {
+	var TableMarkup='<table id="myModifiedTable"  class="visibilityHide">';
+
+	TableMarkup += '<thead><tr>';
+	//...
+	TableMarkup += '</tr></thead>';
+	TableMarkup += '<tbody>';
+	//...
+	TableMarkup += '</tbody>';
+}
+
+}
